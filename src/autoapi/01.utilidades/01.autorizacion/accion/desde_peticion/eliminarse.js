9e0d13ef1ac88039d96cfb98397852892d123ab4
allnulled <todosanulados@gmail.com>
2023-06-03 18:14:16 +0200

    quitados consolelogs de eliminarse

--- src/autoapi/01.utilidades/01.autorizacion/accion/desde_peticion/eliminarse.js
+++ src/autoapi/01.utilidades/01.autorizacion/accion/desde_peticion/eliminarse.js
@@ -4,11 +4,6 @@ module.exports = async function (req, res, next) {
         await this.servidor.ayudante.parsear_cuerpo(req, res);
         const parametros = await this.utilidades.extraer_parametros_de_peticion(req, ["token_de_sesion"], ["post","headers"]);
         const { token_de_sesion } = parametros;
-        console.log(token_de_sesion);
-        console.log(token_de_sesion);
-        console.log(token_de_sesion);
-        console.log(token_de_sesion);
-        console.log(token_de_sesion);
         const resultado = await this.utilidades.autorizacion.accion.eliminarse(token_de_sesion);
         return resultado;
     } catch (error) {
