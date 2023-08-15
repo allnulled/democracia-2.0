0cf130b15d13dd24db9b723d5ff0b063631c3571
allnulled <todosanulados@gmail.com>
2023-08-15 13:51:04 +0200

    preparando base de tests para operaciones crud+auth

---
+++ test/e2e/servicios_de_datos_y_auth/Servicio de datos para «al_eliminar.aplicar_autorizacion».js
@@ -0,0 +1,23 @@
+module.exports = async function (utilidades_de_test) {
+    try {
+
+        const { chai, axios, ruta_de_app, tester, revisor_de_objeto } = utilidades_de_test;
+        const { expect } = chai;
+        const { subtest, iniciar } = tester("Conjunto de tests del servicio de datos para «al_actualizar» + ", { debuga: 0 });
+        const metadatos = {};
+
+        subtest("Subtest contra una url concreta", async function () {
+            try {
+
+            } catch (error) {
+                console.log(error);
+                throw error;
+            }
+        });
+
+        await iniciar();
+
+    } catch (error) {
+        throw error;
+    }
+}
