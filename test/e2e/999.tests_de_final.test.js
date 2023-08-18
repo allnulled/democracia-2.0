02e8d825ae50176046f39a6dc6a59d4a4fb642d0
allnulled <todosanulados@gmail.com>
2023-08-18 11:27:09 +0200

    sin fuerzas fisicas ni mentales, gracias, risperidona y rivotril

--- test/e2e/999.tests_de_final.test.js
+++ test/e2e/999.tests_de_final.test.js
@@ -17,8 +17,10 @@ describe("✔✔✔ Tests de final", function () {
     it("Cancelación de proceso de «democracia-2.0» desde línea de comandos", async function () {
         try {
             const inicio = await utilidades_de_test.democracia20;
-            console.log(inicio)
             inicio.utilidades.terminar_procesos_derivados();
+            setTimeout(function() {
+                inicio.utilidades.terminar_proceso_de_pid();
+            }, 2000);
         } catch (error) {
             throw error;
         }
